11ad1907bc6bd263271a54b78c4a0d027aa22352
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2023-02-09 10:30:35 +0100

    Tuto terminé

--- cliff-height-timer/src/views/History.vue
+++ cliff-height-timer/src/views/History.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container>
     <div className="history">
-      <span>Historique</span>
+      <span>{{histo}}</span>
     </div>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'history-test',
+  name: 'history_vue',
   data: () => {
     return {
       histo: JSON.parse(localStorage.getItem("history"))
